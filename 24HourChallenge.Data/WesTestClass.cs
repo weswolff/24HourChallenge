91d2c1fbd695cc4df110ec7eb98fd517a8903683
weswolff <weswolff@gmail.com>
2020-10-22 10:17:51 -0400

    changed webAPI connection

--- 24HourChallenge.Data/WesTestClass.cs
+++ 24HourChallenge.Data/WesTestClass.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace _24HourChallenge.Data
 {
+<<<<<<< Updated upstream:24HourChallenge.Data/WesTestClass.cs
     class WesTestClass
+=======
+    public class User
+>>>>>>> Stashed changes:24HourChallenge.Data/User.cs
     {
         // This is a comment added into develop branch
     }
--- 24HourChallenge.WebAPI/App_Start/IdentityConfig.cs
+++ 24HourChallenge.WebAPI/App_Start/IdentityConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using _24HourChallenge.WebAPI.Models;
+using _24HourChallenge.Data;
 
 namespace _24HourChallenge.WebAPI
 {
